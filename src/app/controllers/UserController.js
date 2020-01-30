76a481fb5f05c443c6fa351f05e26f3db0bb2b1d
Leonardo Alves <leonardo@gero.com.br>
2020-01-30 15:48:08 -0300

    Create User Authentication with authMiddleware using JWT

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,26 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.findOne({ where: { email: req.body.email } });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
 
-    if (user) {
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
       return res.status(400).json({ error: 'Users already exists.' });
     }
 
@@ -17,12 +33,48 @@ class UserController {
     });
   }
 
-  async delete(req, res) {
-    const user = await User.findOne({ where: { email: req.body.email } });
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-    if (user) {
-      return res.status(400).json({ error: 'Users already exists.' });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
     }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'dbd256cb980517a2b11eeac71f4ef6c6',
+  expiresIn: '7d',
+};
